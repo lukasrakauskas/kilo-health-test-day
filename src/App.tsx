87b0f8b70a785d21e1b6719ec0b8e78888f2bb57
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-03-30 16:59:20 +0300

    Improve skipping question

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AnswersDTO } from 'features/quiz/answersDto';
 import { defaultGetNextQuestion } from 'features/quiz/components/Question/defaultGetNextQuestion';
 import Quiz from 'features/quiz/pages/Quiz';
 import Quizzes from 'features/quiz/pages/Quizzes';
+import { QuizDTO } from 'features/quiz/quizDto';
 import useLocalStorage from 'hooks/useLocalStorage';
 
 function App(): JSX.Element {
@@ -12,33 +13,41 @@ function App(): JSX.Element {
     setAnswers(newAnswers);
   };
 
+  const handleNextQuestion = (
+    quiz: QuizDTO,
+    questionKey: string,
+    answer: string | string[] | null
+  ): string | null => {
+    if (
+      questionKey === 'primary_goal' &&
+      answer !== 'improve_specific_body_areas'
+    ) {
+      const currentIndex = quiz.questions.findIndex(
+        (question) => question.key === questionKey
+      );
+
+      const nextQuestionKey = quiz.questions?.[currentIndex + 1]?.key ?? '';
+
+      return defaultGetNextQuestion(quiz, nextQuestionKey);
+    }
+
+    return defaultGetNextQuestion(quiz, questionKey);
+  };
+
   return (
     <Switch>
       <Route
         path="/:quizSlug/:questionSlug?"
-        render={(routeProps) => (
-          <Quiz
-            slug={routeProps.match.params.quizSlug}
-            onSubmit={handleSubmit}
-            getNextQuestion={(quiz, questionKey, answer) => {
-              if (
-                routeProps.match.params.quizSlug === 'test-day' &&
-                questionKey === 'primary_goal' &&
-                answer === 'improve_specific_body_areas'
-              )
-                return 'body_areas';
-
-              const newQuiz = {
-                ...quiz,
-                questions: quiz.questions.filter(
-                  (question) => question.key !== 'body_areas'
-                ),
-              };
-
-              return defaultGetNextQuestion(newQuiz, questionKey);
-            }}
-          />
-        )}
+        render={(routeProps) => {
+          const slug = routeProps.match.params.quizSlug;
+          return (
+            <Quiz
+              slug={slug}
+              onSubmit={handleSubmit}
+              getNextQuestion={handleNextQuestion}
+            />
+          );
+        }}
       />
       <Route exact path="/" component={Quizzes} />
     </Switch>
