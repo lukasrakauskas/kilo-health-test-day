5153b64137f24fa5eef51ba4376e56f8cc74ee5b
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-03-30 12:19:24 +0300

    Add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { store } from './store/store';
 import App from './App';
@@ -11,7 +12,9 @@ import './index.css';
 render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </StrictMode>,
   document.getElementById('root')
